2f6be4b6b79690636c3e2bd6923c1b7b66ffd8a1
Yuki Watanabe <ukwhatn@gmail.com>
2024-04-26 17:07:22 +0900

    improve

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: '/usr/build/dist',
-        emptyOutDir: true,
+  plugins: [react()],
+  build: {
+    outDir: '/usr/build/dist',
+    emptyOutDir: true,
+  },
+  server: {
+    fs: {
+      cachedChecks: false
+    },
+    proxy: {
+      '/api': {
+        target: 'http://nginx:80',
+        changeOrigin: true
+      }
     }
+  }
 })
