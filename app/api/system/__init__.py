d520c50a4d11faa6369938e4bbb167c1a7e00ac3
Yuki Watanabe <ukwhatn@gmail.com>
2025-03-14 01:18:55 +0900

    wip

--- app/api/system/__init__.py
+++ app/api/system/__init__.py
@@ -3,6 +3,4 @@ from fastapi import APIRouter
 from app.api.system import healthcheck
 
 router = APIRouter()
-
-# ルーターを登録
 router.include_router(healthcheck.router, prefix="/healthcheck", tags=["system"])
