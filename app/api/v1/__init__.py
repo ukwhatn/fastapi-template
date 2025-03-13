77117cfb60b4e540f14fc96fa00b218b9d2cf743
Yuki Watanabe <ukwhatn@gmail.com>
2025-03-14 01:18:58 +0900

    wip

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,7 +3,5 @@ from fastapi import APIRouter
 from app.api.v1 import items, root
 
 router = APIRouter()
-
-# ルーターを登録
 router.include_router(root.router, tags=["root"])
 router.include_router(items.router, prefix="/items", tags=["items"])
