359e38153c20e1881d98941efe363f98d56035fa
Yuki Watanabe <ukwhatn@gmail.com>
2025-10-25 21:27:19 +0900

    ログ修正

--- app/main.py
+++ app/main.py
@@ -36,18 +36,13 @@ app_params: Dict[str, Any] = {
 }
 
 # ロガー設定
-logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger("uvicorn")
 
-if settings.is_development:
-    logger.setLevel(level=logging.DEBUG)
-else:
-    logger.setLevel(level=logging.INFO)
-    # 本番環境ではドキュメントを無効化
-    if settings.is_production:
-        app_params["docs_url"] = None
-        app_params["redoc_url"] = None
-        app_params["openapi_url"] = None
+# 本番環境ではドキュメントを無効化
+if settings.is_production:
+    app_params["docs_url"] = None
+    app_params["redoc_url"] = None
+    app_params["openapi_url"] = None
 
 # New Relic設定
 if settings.is_production and settings.NEW_RELIC_LICENSE_KEY:
